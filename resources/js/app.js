a481d0f60df3c2582cf9ec749ec948848ffca6d8
almunawar12 <rifkialmunawar34@gmail.com>
2026-05-14 23:19:09 +0700

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,8 @@
 import flatpickr from "flatpickr";
 import TomSelect from "tom-select";
-import Alpine from "alpinejs";
 import "./bootstrap";
 import "./../../vendor/power-components/livewire-powergrid/dist/powergrid";
 
 // @ts-ignore
 window.TomSelect = TomSelect;
 window.flatpickr = flatpickr;
-
-// Set Alpine globally before Livewire initializes — Livewire v3 calls Alpine.start()
-window.Alpine = Alpine;
